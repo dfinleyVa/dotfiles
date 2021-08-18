c289a34ca14c4b87aa143bbaf7746398e49919ad
dfinleyVa <74261887+dfinleyVa@users.noreply.github.com>
2021-08-18 12:30:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 
 if [[ ! -f "$HOME/.cfg" ]]; then
   echo ".cfg" >> .gitignore
-  git clone --bare https://github.com/jaredzieche/dotfiles.git $HOME/.cfg --recurse-submodules
+  git clone --bare https://github.com/dfinleyVa/dotfiles.git $HOME/.cfg --recurse-submodules
   /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME config --local status.showUntrackedFiles no
   /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME checkout
 fi
